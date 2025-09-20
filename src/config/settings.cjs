d54b51094f7ec136a79dab0e35bb498e1150cf83
xtharindudranasingha-svg <x.tharindu.d.ranasingha@gmail.com>
2025-09-20 23:29:11 +0530

    Create settings.cjs

---
+++ src/config/settings.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  "PREFIX": ".",
+  "OWNERS": [
+    "1234567890@s.whatsapp.net"
+  ],
+  "PORT": "3000",
+  "AUTOREADSTATUS": true,
+  "MODE": "public",
+  "BOTNUMBER": "94786073208",
+  "OMDBAPI_KEY": "76cb7f39",
+  "READCMD": true,
+  "AUTOVOICE": false,
+  "AUTOSTICKER": true,
+  "AUTOREPLY": true,
+  "AUTOREACT": false,
+  "WELCOME": false,
+  "ANTIBAD": true,
+  "ANTIBOT": true,
+  "ANTILINK": false,
+  "ANTICALL": false,
+  "ANTIDELETE": true,
+  "ALLWAYSONLINE": true,
+  "MOROCCOBLOCK": true,
+  "OWNERNAME": "Tharindu",
+  "NEWSJID": "120363393420637374@g.us",
+  "MOVIEJID": false,
+  "ANIMEJID": "120363367650594508@g.us",
+  "AUTOTYPING": false,
+  "AUTORECORDING": true,
+  "AUTONEWS": false,
+  "OWNERREACT": false,
+  "EMOJI": "🏷",
+  "AIMODE": false,
+  "BANNED": [],
+  "SUDO": [
+    "94786073208"
+  ]
+};
